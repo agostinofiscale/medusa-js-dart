aaaa263b61941cb5d853cfd83cc8088248652da0
Agostino Fiscale <agostinofiscale@gmail.com>
2024-02-25 15:04:12 +0100

    First commit

---
+++ example/medusa_js_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:medusa_js_dart/medusa_js_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/medusa_js_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:medusa_js_dart/medusa_js_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
