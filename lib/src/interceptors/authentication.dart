12813c149519de8aa4d95d278d1398b2ca6e6378
Agostino Fiscale <agostinofiscale@gmail.com>
2024-03-10 02:14:56 +0100

    feat: improve authentication interceptor

--- lib/src/interceptors/authentication.dart
+++ lib/src/interceptors/authentication.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:medusa_js_dart/src/medusa.dart';
 import 'package:medusa_js_dart/src/models/enums/authentication_type.dart';
@@ -15,12 +17,14 @@ class AuthenticationInterceptor extends Interceptor {
         options.headers['Authorization'] =
             'Bearer ${medusa.configuration.apiKey}';
       case AuthenticationType.jwt:
+        final Domain domain = options.uri.path.contains(Domain.admin.toString())
+            ? Domain.admin
+            : Domain.store;
         options.headers['Authorization'] =
-            'Bearer ${medusa.getJwtToken(Domain.admin)}';
+            'Bearer ${medusa.getJwtToken(domain)}';
       case AuthenticationType.cookie:
-
-        /// TODO: How to handle cookies?
-        break;
+        options.headers[HttpHeaders.cookieHeader] =
+            'connect.sid=${medusa.configuration.cookieToken}';
       default:
         break;
     }
@@ -28,15 +32,50 @@ class AuthenticationInterceptor extends Interceptor {
     super.onRequest(options, handler);
   }
 
+  /// This method intercepts the response and sets auth tokens.
+  ///
+  /// TODO: Should we split this into two different interceptors?
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    if (response.realUri.path.contains('/admin/auth')) {
-      final String? cookie = response.headers.map['set-cookie']?.first;
+    if (response.requestOptions.method != 'POST' ||
+        response.statusCode != 200) {
+      return super.onResponse(response, handler);
+    }
 
-      if (cookie != null) {
-        medusa.configuration = medusa.configuration.copyWith(
-          cookieToken: cookie,
-        );
+    /// Cookie support for both store and admin authentication,
+    /// as the cookie is set in the same way for both.
+    ///
+    /// TODO: Should we split this into two different variables?
+    if (response.realUri.path.contains('/auth')) {
+      final String? cookie =
+          response.headers.map[HttpHeaders.setCookieHeader]?.first;
+
+      final String? cookieToken = cookie?.split(';').first;
+
+      final String? token = cookieToken?.split('=').last;
+
+      if (token != null) {
+        medusa.setCookieToken(token);
+      }
+    }
+
+    /// JWT support
+    if (response.realUri.path.contains('/auth/token')) {
+      String? jwtToken;
+
+      final Map<String, dynamic> data = response.data as Map<String, String>;
+
+      if (data.containsKey('access_token')) {
+        jwtToken = data['access_token'] as String;
+      }
+
+      if (jwtToken != null) {
+        final Domain domain =
+            response.realUri.path.contains(Domain.admin.toString())
+                ? Domain.admin
+                : Domain.store;
+
+        medusa.setJwtToken(domain, jwtToken);
       }
     }
 
